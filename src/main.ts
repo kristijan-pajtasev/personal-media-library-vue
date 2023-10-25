9259b063dbd073710afe18b3e6600a5197c0f4a5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 23:14:07 +0100

    init book store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import {createStore} from 'vuex'
 import App from './App.vue'
 import router from './router'
+import BooksStore from './store/book';
+
+const store = createStore({
+    modules: {
+        books: BooksStore
+    }
+});
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store);
 
 app.mount('#app')
---
+++ src/store/book/index.js
@@ -0,0 +1,15 @@
+const TodoStore = {
+  namespaced: true,
+  state() {
+    return {
+    }
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  }
+}
+
+export default TodoStore
