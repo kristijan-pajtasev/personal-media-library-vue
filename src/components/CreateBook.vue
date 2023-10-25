bae063ee70fdf83288ec161b8d2ba157e7669506
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 23:44:50 +0100

    form structure

--- src/components/CreateBook.vue
+++ src/components/CreateBook.vue
@@ -1,17 +1,34 @@
-<template>
-  <div>create book</div>
-</template>
-
 <script>
 export default {
   data() {
-    return {}
+    return {
+      title: "",
+      author: "",
+    }
   },
   mounted() {
     console.log("create book component")
-  }
+  },
+  methods: {}
 }
 </script>
+
+<template>
+  <div>
+    <div>add book</div>
+    <form>
+      <div>
+        <input v-model="title" placeholder="title" type="text"/>
+      </div>
+      <div>
+        <input v-model="author" placeholder="author" type="text"/>
+      </div>
+      <div>
+        <button>add book</button>
+      </div>
+    </form>
+  </div>
+</template>
 
 <style scoped>
 </style>
