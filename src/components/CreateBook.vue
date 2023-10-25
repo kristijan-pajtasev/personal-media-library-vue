08bffe4154ef45ef106ec896d55c611ccae519a2
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 23:42:11 +0100

    init create book

---
+++ src/components/CreateBook.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>create book</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log("create book component")
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/CreateBookView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CreateBook from '../components/CreateBook.vue'
+</script>
+
+<template>
+  <main>
+    <CreateBook />
+  </main>
+</template>
