b8355d45684207db414d73b150b0691291e36fdf
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-25 23:05:58 +0100

    home links

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,14 @@ export default {
 </script>
 
 <template>
-  <div>home component</div>
+  <div>
+    <div>
+      <a href="/vinyl">Vinyls</a>
+    </div>
+    <div>
+      <a href="/book">Books</a>
+    </div>
+  </div>
 </template>
 
 <style>
