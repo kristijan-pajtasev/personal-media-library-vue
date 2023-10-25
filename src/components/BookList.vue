eca04f256029d98b4ed3aaf269defab463dbdfe5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-26 00:06:57 +0100

    styling

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -9,12 +9,36 @@ export default {
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li v-for="book in books" :key="book.id">
+  <div class="BookList">
+    <ul class="BookList__list">
+      <li v-for="book in books" :key="book.id" class="BookList__listItem">
         <div>{{book.title}}</div>
         <div>{{ book.author }}</div>
       </li>
     </ul>
   </div>
 </template>
+
+<style scoped>
+.BookList {
+  max-width: 800px;
+  margin: auto;
+}
+
+.BookList__list {
+  list-style: none;
+  padding: 0;
+}
+
+.BookList__listItem {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem;
+  border-bottom: thin solid hsla(160, 100%, 37%, 1)
+}
+
+.BookList__listItem>* {
+  flex: 1;
+}
+</style>
