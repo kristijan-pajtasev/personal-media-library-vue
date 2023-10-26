801ba5ecf9fb4f235d0aef521aadc2882187a5e9
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-26 13:07:49 +0100

    redirect on click

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,31 +2,65 @@
 export default {
   data() {
     return {}
+  },
+  methods: {
+    goToBooks() {
+      this.$router.push({name: "book"});
+    },
+    goToVinyl() {
+      this.$router.push({name: "vinyl"});
+    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <div>
+  <div class="Home__menuContainer">
+    <div @click="goToVinyl">
       <div>
-        <img src="/images/download.png" alt="viny icon image"/>
+        <img class="Home__image" src="/images/download.png" alt="viny icon image"/>
       </div>
       <div>
-        <a href="/vinyl">Vinyls</a>
+        <a class="Home__link" href="/vinyl">Vinyls</a>
       </div>
     </div>
-    <div>
+    <div @click="goToVinyl">
       <div>
-        <img src="/images/book.png" alt="book icon image"/>
+        <img class="Home__image" src="/images/book.png" alt="book icon image"/>
       </div>
       <div>
-        <a href="/book">Books</a>
+        <a class="Home__link" href="/book">Books</a>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+.Home__menuContainer {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  max-width: 600px;
+  gap: 1rem;
+  margin: auto;
+}
 
+.Home__menuContainer>* {
+  flex: 1;
+  text-align: center;
+  cursor: pointer;
+}
+
+.Home__menuContainer>*:hover {
+  background: #ccc;
+}
+
+.Home__image {
+  width: 100%;
+}
+
+.Home__link {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
 </style>
