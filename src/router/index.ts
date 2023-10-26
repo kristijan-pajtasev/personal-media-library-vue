be637961b188ad1b4483ca6c0afa199b4ce113d9
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-27 00:21:07 +0100

    edit route

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,14 @@ const router = createRouter({
       component: () => import('../views/CreateBookView.vue')
     },
     {
+      path: '/book/:id/edit',
+      name: 'editBook',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/EditBookView.vue')
+    },
+    {
       path: '/vinyl/create',
       name: 'createVinyl',
       // route level code-splitting
