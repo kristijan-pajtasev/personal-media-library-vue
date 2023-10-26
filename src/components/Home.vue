a8b49ee22fce35598c8ac257c35a32ddbd37d806
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-26 13:00:22 +0100

    iamges added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,10 +9,20 @@ export default {
 <template>
   <div>
     <div>
-      <a href="/vinyl">Vinyls</a>
+      <div>
+        <img src="/images/download.png" alt="viny icon image"/>
+      </div>
+      <div>
+        <a href="/vinyl">Vinyls</a>
+      </div>
     </div>
     <div>
-      <a href="/book">Books</a>
+      <div>
+        <img src="/images/book.png" alt="book icon image"/>
+      </div>
+      <div>
+        <a href="/book">Books</a>
+      </div>
     </div>
   </div>
 </template>
