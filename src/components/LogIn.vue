08d02ca30822e24bdac5acdf1a2a1193545fbd8d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-29 00:57:44 +0100

    using ref values

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -13,33 +13,56 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  // type inference enabled
-  props: {},
-  mounted() {
-  },
-  data() {
-    return {
-      email: "",
-      password: ""
-    }
-  },
-  computed: {},
-  methods: {
-    submitHandler() {
-      const router = this.$router;
-      this.$store.dispatch("user/logIn", {
-        email: this.email,
-        password: this.password
+<script setup lang="ts">
+
+import {ref, computed, defineProps} from 'vue'
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+
+const store = useStore();
+const router = useRouter();
+
+const email = ref("");
+const password = ref("");
+
+// functions
+function submitHandler() {
+      store.dispatch("user/logIn", {
+        email: email.value,
+        password: password.value
       }).then(
           () => {
             router.push({name: "home"})
           }
       )
     }
-  }
-}
+
+// export default {
+//   // type inference enabled
+//   props: {},
+//   mounted() {
+//   },
+//   data() {
+//     return {
+//       email: "",
+//       password: ""
+//     }
+//   },
+//   computed: {},
+//   methods: {
+//     submitHandler() {
+//       const router = this.$router;
+//       this.$store.dispatch("user/logIn", {
+//         email: this.email,
+//         password: this.password
+//       }).then(
+//           () => {
+//             router.push({name: "home"})
+//           }
+//       )
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
