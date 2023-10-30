b56d352f7465d0522dfae9e43d63e7c6693a6040
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-30 00:59:57 +0000

    init book list

--- src/components/BookListContainer.vue
+++ src/components/BookListContainer.vue
@@ -1,40 +1,47 @@
-<script lang="ts">
+<script setup lang="ts">
 import BookList from './BookList.vue';
-export default {
-  components: {BookList},
-  data() {
-    return {
-      author: "",
-      title: ""
-    }
-  },
-  mounted() {
-    this.$store.dispatch('book/getBooks')
-  },
-  computed: {
-    books() {
-      const books = this.$store.getters["book/getAllBooks"];
+import {ref, computed, onMounted} from 'vue'
+import {useStore} from 'vuex';
+import {useRouter} from 'vue-router';
 
-      if(!books) return;
+const store = useStore();
+const router = useRouter();
 
-      const author = this.author
-      const title = this.title
-      const filteredBooks = books.filter(book => {
-        if(author.length > 0) {
-          if(!book.author.toLowerCase().includes(author.toLowerCase())) return false;
-        }
-        if(title.length > 0) {
-          if(!book.title.toLowerCase().includes(title.toLowerCase())) return false;
-        }
-        return true
-      })
-      return filteredBooks
-    },
-    loading() {
-      return this.$store.getters['book/isLoading']
-    }
-  }
-}
+// export default {
+//   components: {BookList},
+//   data() {
+//     return {
+//       author: "",
+//       title: ""
+//     }
+//   },
+//   mounted() {
+//     this.$store.dispatch('book/getBooks')
+//   },
+//   computed: {
+//     books() {
+//       const books = this.$store.getters["book/getAllBooks"];
+//
+//       if(!books) return;
+//
+//       const author = this.author
+//       const title = this.title
+//       const filteredBooks = books.filter(book => {
+//         if(author.length > 0) {
+//           if(!book.author.toLowerCase().includes(author.toLowerCase())) return false;
+//         }
+//         if(title.length > 0) {
+//           if(!book.title.toLowerCase().includes(title.toLowerCase())) return false;
+//         }
+//         return true
+//       })
+//       return filteredBooks
+//     },
+//     loading() {
+//       return this.$store.getters['book/isLoading']
+//     }
+//   }
+// }
 </script>
 
 <template>
