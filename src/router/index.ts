b20150b91a77632a003b8b5e156eba506f7bd740
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-11-08 00:44:03 +0000

    book view component

--- src/router/index.ts
+++ src/router/index.ts
@@ -56,6 +56,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LogInView.vue')
     },
+    // {
+    //   path: '/vinyl',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/BookView.vue'),
+    //   children: []
+    // },
     {
       path: '/vinyl',
       // route level code-splitting
